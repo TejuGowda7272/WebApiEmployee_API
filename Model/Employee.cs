60eb9f08360ce6576e3a17152b853f8771e81783
Tejaswini G H <tejugowda363@gmail.com>
2025-08-28 11:01:38 +0530

    Made ChangesAdd project files.

---
+++ Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiEmployee.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+       
+        public required string Name { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [EmailAddress, Required]
+        public string Email { get; set; } 
+
+        [Required]
+        public string? Address { get; set; }
+    }
+}
